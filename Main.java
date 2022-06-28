234c610ed69fe2ee95cfeea3c2863124740d26d3
Albion Qerreti <albionqerreti@gmail.com>
2022-06-29 01:41:52 +0200

    init

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static double sum (double a, double b) {
+
+        return a+b;
+    }
+    
+    public static void main (String[] args) {
+        System.out.println(sum(5,8));
+        
+
+
+    }
+}
